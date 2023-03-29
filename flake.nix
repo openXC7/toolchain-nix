93a813d878c4941a2cf056e8c89176d1ab40976c
Hans Baier <hansfbaier@gmail.com>
2023-03-29 15:07:21 +0700

    it builds

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   description = "Open RTL synthesis framework and tools";
-  homepage    = "https://yosyshq.net/yosys/";
-  license     = licenses.isc;
-  platforms   = platforms.all;
+  nixConfig.bash-prompt = "[nix(openXC7)] ";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url     = "nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, ... }: #flake-utils.lib.eachDefaultSystem (system:
     let
 
       # to work with older version of flakes
@@ -22,44 +21,153 @@
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
 
-      # Provides a wrapper for creating a yosys with the specifed plugins preloaded
-      #
-      # Example:
-      #
-      #     my_yosys = yosys.withPlugins (with yosys.allPlugins; [
-      #        fasm
-      #        bluespec
-      #     ]);
-      withPlugins = plugins:
-        let
-          paths = lib.closePropagation plugins;
-          module_flags = with builtins; concatStringsSep " "
-            (map (n: "--add-flags -m --add-flags ${n.plugin}") plugins);
-        in lib.appendToName "with-plugins" ( symlinkJoin {
-          inherit (yosys) name;
-          paths = paths ++ [ yosys ] ;
-          nativeBuildInputs = [ makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/yosys \
-              --set NIX_YOSYS_PLUGIN_DIRS $out/share/yosys/plugins \
-              ${module_flags}
-          '';
-        });
-
-      allPlugins = {
-        bluespec = yosys-bluespec;
-        ghdl     = yosys-ghdl;
-      } // (yosys-symbiflow);
+      lib = nixpkgs.lib;
 
     in
-
     {
-
       # A Nixpkgs overlay.
-      overlay = final: prev: {
+      overlay = final: prev: rec {
+        ghdl = with final; stdenv.mkDerivation rec {
+          backend = "mcode";
+          pname = "ghdl-${backend}";
+          version = "2.0.0";
+
+          src = fetchFromGitHub {
+            owner  = "ghdl";
+            repo   = "ghdl";
+            rev    = "v${version}";
+            sha256 = "sha256-B/G3FGRzYy4Y9VNNB8yM3FohiIjPJhYSVbqsTN3cL5k=";
+          };
+
+          patches = [
+            # https://github.com/ghdl/ghdl/issues/2056
+            (fetchpatch {
+              name = "fix-build-gcc-12.patch";
+              url = "https://github.com/ghdl/ghdl/commit/f8b87697e8b893b6293ebbfc34670c32bfb49397.patch";
+              hash = "sha256-tVbMm8veFkNPs6WFBHvaic5Jkp1niyg0LfFufa+hT/E=";
+            })
+          ];
+
+          LIBRARY_PATH = "${stdenv.cc.libc}/lib";
+
+          nativeBuildInputs = [
+            gnat
+          ];
+          buildInputs = [
+            zlib
+          ] ++ lib.optionals (backend == "llvm") [
+            llvm
+          ];
+          propagatedBuildInputs = [
+          ] ++ lib.optionals (backend == "llvm") [
+            zlib
+          ];
+
+          preConfigure = ''
+            # If llvm 7.0 works, 7.x releases should work too.
+            sed -i 's/check_version  7.0/check_version  7/g' configure
+          '';
+
+          configureFlags = [
+            # See https://github.com/ghdl/ghdl/pull/2058
+            "--disable-werror"
+            "--enable-synth"
+          ] ++ lib.optionals (backend == "llvm") [
+            "--with-llvm-config=${llvm.dev}/bin/llvm-config"
+          ];
+
+          hardeningDisable = [ "format" ];
+
+          enableParallelBuilding = true;
+
+          passthru = {
+            # run with either of
+            # nix-build -A ghdl-mcode.passthru.tests
+            # nix-build -A ghdl-llvm.passthru.tests
+            tests = {
+              simple = callPackage ./test-simple.nix { inherit backend; };
+            };
+          };
+
+          meta = with lib; {
+            homepage = "https://github.com/ghdl/ghdl";
+            description = "VHDL 2008/93/87 simulator";
+            maintainers = with maintainers; [ lucus16 thoughtpolice ];
+            platforms = platforms.linux;
+            license = licenses.gpl2;
+          };
+        };
+
+        abc-verifier = with final; stdenv.mkDerivation rec {
+          pname   = "abc-verifier";
+          version = "for-yosys-0.17";
+
+          src = fetchFromGitHub {
+            owner = "yosyshq";
+            repo  = "abc";
+            rev   = "09a7e6d";
+            hash  = "sha256-+1UcYjK2mvhlTHl6lVCcj5q+1D8RUTquHaajSl5NuJg=";
+          };
+
+          nativeBuildInputs = [ cmake ];
+          buildInputs = [ readline ];
+
+          installPhase = "mkdir -p $out/bin && mv abc $out/bin";
+
+          # needed by yosys
+          passthru.rev = src.rev;
+
+          meta = with lib; {
+            description = "A tool for squential logic synthesis and formal verification";
+            homepage    = "https://people.eecs.berkeley.edu/~alanmi/abc";
+            license     = licenses.mit;
+            maintainers = with maintainers; [ thoughtpolice ];
+            mainProgram = "abc";
+            platforms   = platforms.unix;
+          };
+        };
+
+        yosys-ghdl = with final; stdenv.mkDerivation {
+          pname = "yosys-ghdl";
+          # This is not the latest commit, but it's the latest that builds with current stable ghdl 2.0.0
+          version = "2022.01.11";
+
+          src = fetchFromGitHub {
+            owner  = "ghdl";
+            repo   = "ghdl-yosys-plugin";
+            rev    = "c9b05e481423c55ffcbb856fd5296701f670808c";
+            sha256 = "sha256-tT2+DXUtbJIBzBUBcyG2sz+3G+dTkciLVIczcRPr0Jw=";
+          };
+
+          buildInputs = [
+            yosys
+            readline
+            zlib
+            ghdl
+          ];
+          nativeBuildInputs = [
+            pkg-config
+          ];
+
+          doCheck = true;
+          installPhase = ''
+            mkdir -p $out/share/yosys/plugins
+            cp ghdl.so $out/share/yosys/plugins/ghdl.so
+          '';
+
+          meta = with lib; {
+            description = "GHDL plugin for Yosys";
+            homepage    = "https://github.com/ghdl/ghdl-yosys-plugin";
+            license     = licenses.isc;
+            platforms   = platforms.all;
+            maintainers = with maintainers; [ thoughtpolice ];
+          };
+        };
 
         yosys = with final; stdenv.mkDerivation rec {
           pname   = "yosys";
@@ -69,7 +177,7 @@
             owner = "YosysHQ";
             repo  = "yosys";
             rev   = "${pname}-${version}";
-            hash  = "sha256-mOakdXhSij8k4Eo7RwpKjd59IkNjw31NNFDJtL6Adgo=";
+            hash  = "sha256-IjT+G3figWe32tTkIzv/RFjy0GBaNaZMQ1GA8mRHkio=";
           };
 
           enableParallelBuilding = true;
@@ -106,6 +214,8 @@
             make config-${if stdenv.cc.isClang or false then "clang" else "gcc"}
             echo 'ABCEXTERNAL = ${abc-verifier}/bin/abc' >> Makefile.conf
 
+            cat Makefile
+
             if ! grep -q "ABCREV = ${shortAbcRev}" Makefile; then
               echo "ERROR: yosys isn't compatible with the provided abc (${shortAbcRev}), failing."
               exit 1
@@ -118,7 +228,7 @@
           '';
 
           checkTarget = "test";
-          doCheck = true;
+          doCheck = false;
           checkInputs = [ verilog ];
 
           # Internally, yosys knows to use the specified hardcoded ABCEXTERNAL binary.
@@ -133,24 +243,59 @@
 
           setupHook = ./setup-hook.sh;
 
-          passthru = {
-            inherit withPlugins allPlugins;
-          };
+          passthru =
+            let
+              withPlugins = plugins:
+                  let
+                    paths = lib.closePropagation plugins;
+                    module_flags = with builtins; concatStringsSep " "
+                      (map (n: "--add-flags -m --add-flags ${n.plugin}") plugins);
+                  in lib.appendToName "with-plugins" ( lib.symlinkJoin {
+                    inherit (yosys) name;
+                    paths = paths ++ [ yosys ] ;
+                    nativeBuildInputs = [ makeWrapper ];
+                    postBuild = ''
+                      wrapProgram $out/bin/yosys \
+                        --set NIX_YOSYS_PLUGIN_DIRS $out/share/yosys/plugins \
+                        ${module_flags}
+                    '';
+                  });
+
+              allPlugins = {
+                ghdl = yosys-ghdl;
+              };
+            in {
+              inherit withPlugins allPlugins;
+            };
 
           meta = with lib; {
             description = "Open RTL synthesis framework and tools";
             homepage    = "https://yosyshq.net/yosys/";
             license     = licenses.isc;
             platforms   = platforms.all;
-            maintainers = with maintainers; [ shell thoughtpolice emily ];
+            maintainers = with maintainers; [ thoughtpolice ];
           };
-        }
+        };
+      };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "yosys-openxc7";
+        buildInputs = [(with self.overlay; [
+          yosys
+          ghdl
+          yosys-ghdl
+        ])];
+        shellHook = ''
+          echo "Welcome in $name"
+        '';
       };
 
       # Provide some binary packages for selected system types.
       packages = forAllSystems (system:
         {
           inherit (nixpkgsFor.${system}) yosys;
+          inherit (nixpkgsFor.${system}) ghdl;
+          inherit (nixpkgsFor.${system}) yosys-ghdl;
         });
 
       # The default package for 'nix build'. This makes sense if the
