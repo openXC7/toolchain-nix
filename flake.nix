5e94912496f89b3917a7f2cc3bb92fd59bee70d9
Jack Leightcap <jack@leightcap.com>
2023-09-06 18:44:42 -0400

    build: add backends via callPackage
    Co-authored-by: Jason Odoom <jasonodoom@gmail.com>
    Co-authored-by: Anish Lakhwara <anish+git@lakhwara.com>
    Co-authored-by: Dominic Mills <dominic.millz27@gmail.com>
    Co-authored-by: Albert Chae <albertchae@users.noreply.github.com>
    Co-authored-by: Jack Leightcap <jack@leightcap.com>
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- flake.nix
+++ flake.nix
@@ -371,56 +371,10 @@
         };
 
         nextpnr-xilinx-chipdb = {
-          artiz7 = _nextpnr-xilinx-chipdb;   # .override { backend = "artiz7"; };
-          kintex7 = _nextpnr-xilinx-chipdb;  # .override { backend = "kintex7"; };
-          spartan7 = _nextpnr-xilinx-chipdb; # .override { backend = "spartan7"; };
-          zynq7 = _nextpnr-xilinx-chipdb;    # .override { backend = "zynq7"; };
-        };
-
-        _nextpnr-xilinx-chipdb = with prev; stdenv.mkDerivation rec {
-          pname = "nextpnr-xilinx-chipdb";
-          version = nextpnr-xilinx.version;
-          backend = "zynq7";
-
-          src = "${nextpnr-xilinx.outPath}/usr/share/nextpnr/external/prjxray-db";
-
-          inherit (pkgs) coreutils findutils gnused gnugrep;
-          buildInputs = [ prjxray nextpnr-xilinx pypy3 coreutils findutils gnused gnugrep ];
-          buildPhase = ''
-            mkdir -p $out
-            find ${src}/ -type d -name "*-*" -mindepth 1 -maxdepth 2 |\
-              sed -e 's,.*/\(.*\)-.*$,\1,g' -e 's,\./,,g' |\
-              sort |\
-              uniq >\
-            $out/footprints.txt
-
-            for i in `cat $out/footprints.txt`
-            do
-                if   [[ $i = xc7a* ]]; then ARCH=artix7 
-                elif [[ $i = xc7k* ]]; then ARCH=kintex7
-                elif [[ $i = xc7s* ]]; then ARCH=spartan7
-                elif [[ $i = xc7z* ]]; then ARCH=zynq7
-                else 
-                  echo "unsupported architecture for footprint $i"
-                  exit 1
-                fi
-
-                if [[ $ARCH != "${backend}" ]]; then
-                  continue
-                fi
-
-                FIRST_SPEEDGRADE_DIR=`ls -d ${src}/$ARCH/$i-* | sort -n | head -1`
-                FIRST_SPEEDGRADE=`echo $FIRST_SPEEDGRADE_DIR | tr '/' '\n' | tail -1`
-                pypy3.9 ${nextpnr-xilinx}/usr/share/nextpnr/python/bbaexport.py --device $FIRST_SPEEDGRADE --bba $i.bba 2>&1
-                bbasm -l $i.bba $out/$i.bin
-            done
-          '';
-
-          # FIXME(jleightcap): the above buildPhase is adapated from a `builder`; which combines the process of
-          # compiling assets along with installing those assets to `$out`.
-          # These steps should be untangled, ideally - for now just use the buildPhase and disable the (empty)
-          # installPhase.
-          dontInstall = true;
+          artiz7 = prev.callPackage ./nix/nextpnr-xilinx-chipdb.nix { backend = "artiz7"; };
+          kintex7 = prev.callPackage ./nix/nextpnr-xilinx-chipdb.nix { backend = "kintex7"; };
+          spartan7 = prev.callPackage ./nix/nextpnr-xilinx-chipdb.nix { backend = "spartan7"; };
+          zynq7 = prev.callPackage ./nix/nextpnr-xilinx-chipdb.nix { backend = "zynq7"; };
         };
       };
 
