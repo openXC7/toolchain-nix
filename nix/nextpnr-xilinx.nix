3c39071914b7b238667cf5f8bcb245cfd24efd1c
Hans Baier <hansfbaier@gmail.com>
2023-12-21 11:40:53 +0700

    push latest bugfix

--- nix/nextpnr-xilinx.nix
+++ nix/nextpnr-xilinx.nix
@@ -7,8 +7,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "openXC7";
     repo = "nextpnr-xilinx";
-    rev = "8120acd7c036cd87af2f1169e9c555546ad7bded";
-    hash = "sha256-6j9h9yYIVCm0hZlVi8lNDfbivKAmZxZ/kptSKIMziOE=";
+    rev = "670dee4cb4b3dc572f0e1f8a4c8e77ec1c52e7a1";
+    hash = "sha256-3jNBSX4Hc85sIrL3On3cP71mB00FoEJgliTecx96fRE=";
     fetchSubmodules = true;
   };
 
