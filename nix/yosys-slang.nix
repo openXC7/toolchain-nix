ca0a6f3efd06285f8f6f6a031f7d2257ad480714
gitRaiku <raraiku@proton.me>
2026-06-05 00:26:23 +0200

    Finish yosys-slang integration
    Signed-off-by: gitRaiku <raraiku@proton.me>

--- nix/yosys-slang.nix
+++ nix/yosys-slang.nix
@@ -1,26 +1,31 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, cmake
+, yosys
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "yosys-slang";
   plugin = "slang";
-  version = "08bb13bd128c73b79455f4ea344a1785e0a342e9"
+  version = "08bb13bd128c73b79455f4ea344a1785e0a342e9";
 
   src = fetchFromGitHub {
     owner = "povik";
     repo  = "yosys-slang";
     rev   = "${finalAttrs.version}";
-    hash  = "sha256-PODeQL8n34Ekq4NQJ5/w9pTkfkBc77osZc7JzEOUk6I=";
-    fetchSubmodules = false;
+    hash  = "sha256-+elMF8aCaBF1FWWHuXmbN6GsRcpd/+vrJsfmm3LNMX8=";
+    # deepClone = true;
+    fetchSubmodules = true;
   };
 
+
   nativeBuildInputs = [
-    pkg-config
+    cmake
   ];
 
   buildInputs = [
+    yosys
   ];
 
   buildPhase = ''
@@ -35,17 +40,17 @@ stdenv.mkDerivation (finalAttrs: {
   doCheck = true;
   checkPhase = ''
      runHook preCheck
-     # echo "module tester(); endmodule;" > tester.sv
-     # yosys -p "plugin -i build/release/systemverilog-plugin/systemverilog.so;\
-     #          read_systemverilog tester.sv"
+     echo "module tester(); endmodule;" > tester.sv
+     yosys -p "plugin -i ./slang.so; read_slang tester.sv"
      runHook postCheck
   '';
 
   installPhase = ''
     runHook preInstall
-    #mkdir -p $out/share/yosys/plugins
-    #cp ./build/release/systemverilog-plugin/systemverilog.so \
-    #       $out/share/yosys/plugins/systemverilog.so
+
+    mkdir -p $out
+    cp ./slang.so $out/slang.so
+
     runHook postInstall
   '';
 
